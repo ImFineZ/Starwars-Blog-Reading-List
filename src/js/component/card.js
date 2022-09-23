fd2df7e0498300fbd4a8ff2ec6bb9572db6f8a10
ImFineZ <greivinrodriguez@icloud.com>
2022-09-23 04:16:16 +0000

    Actualizacion del navbar

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,31 +1,61 @@
-import React, { Component } from "react";
-/* import { Outlet } from "react-router";
+/* import React, { Component } from "react"; */
+import React from "react";
+import PropType from "prop-types";
+
+
+
+export const Card = (props) => {
+  /* const [addCharacters, setgetcharacters] = useState([""]);
+
+  const getCharacters = () => {
+    fetch("https://www.swapi.tech/api/people/")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
  */
-export const Card = () => {
-
-
-    
   return (
-    <>
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <div className="card-text">
-        {`Gender:
-        Age:
-        Color Eye:`}
-          </div>
+    
+    <div className="card col-4 mx-2">
+      <img
+        src="https://www.prensalibre.com/wp-content/uploads/2019/11/luke-skywalker-644x362.jpg?quality=52"
+        className="card-img-top"
+        alt="..."
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.name}</h5>
+        <h6>Gender:{props.gender}</h6>
+        <br></br>
+        <h6>Hair Color:{props.hairColor}</h6>
+        <br></br>
+        <h6>Eye-Color:{props.eyeColor}</h6>
+        <br></br>
+        <div className="d-flex">
           <div>
             <a href="#" className="btn btn-primary">
               Go somewhere
             </a>
-            <a href="#" className="btn btn-primary d-flex flex-grow-1">
-              <ion-icon name="heart-outline"></ion-icon>
+          </div>
+          <div className="">
+            <a href="#" className="btn btn-warning mx-5">
+              <i className="bi bi-suit-heart"></i>
             </a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
+};
+Card.propTypes = {
+  img: PropType.string,
+  hairColor: PropType.string,
+  eyeColor: PropType.string,
+  name: PropType.string,
+  description: PropType.string,
+  Link: PropType.string,
+  desLink: PropType.string,
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,25 +1,60 @@
-import React from "react";
+/* import React from "react"; */
 import { Card } from "../component/card";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<>
-	<div className="container-sm">
-		<div className="row">
-			<div className="card ">
-			<h1>Character</h1>
-				<Card></Card>
-			</div>
-		</div>
-	</div>
-	<div className="container-sm">
-		<div className="row">
-			<div className="card ">
-			<h1>Planets</h1>
-				<Card></Card>
-			</div>
-		</div>
-	</div>
-	</>
-);
+export const Home = () => {
+  const [addCharacters, setgetcharacters] = useState([""]);
+
+  const getCharacters = () => {
+    fetch("https://www.swapi.tech/api/people/")
+      .then((res) => res.json())
+      .then((data) => setgetcharacters(data))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+  return(
+  <>
+    <div className="container">
+      <h1>Character</h1>
+      <div className="overflow-auto">
+        <div className="d-flex">
+
+		{/*   {img.map((singleimg, i) => {
+          return (
+            <Cards key={i}
+              img={singleimg}
+              alt={alt[i]}
+              tittle={tittle[i]}
+              description={description[i]}
+              link={link[i]}
+              desLink={desLink[i]}
+            />
+          );
+        })} */}
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+      </div>
+    </div>
+    <div className="container">
+      <h1>Planets</h1>
+      <div className="overflow-auto">
+        <div className="d-flex">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+      </div>
+    </div>
+  </>
+  );
+};
